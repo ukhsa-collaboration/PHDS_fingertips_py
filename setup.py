7abc96c47ff99086356972eeb155849e9e9a8396
russell-phe <russell.plunkett@phe.gov.uk>
2017-12-28 15:38:48 +0000

    Added setup.py Changed structure to be deployment ready Added all functions to __init__.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='fingertips_py',
+    version='0.1',
+    packages=['fingertips_py'],
+    url='https://gitlab.phe.gov.uk/Russell.Plunkett/fingertips_py.git',
+    license='',
+    author='Public Health England',
+    author_email='russell.plunkett@phe.gov.uk',
+    description='This is a python package to interact with Public Health England\'s Fingertips data tool. '
+                'This can be used to load data from the Fingertips API into python for further manipulation.',
+    install_requires=['pandas>=0.18.1']
+)
