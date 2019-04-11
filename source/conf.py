f742d0b184fbaf77797f083d5af56dfe9d3dab30
russell.plunkett <russell.plunkett@phe.gov.uk>
2019-04-11 10:34:44 +0100

    Changed docs name again Added master_doc to conf.py

--- source/conf.py
+++ source/conf.py
@@ -78,6 +78,8 @@ html_theme_options = {
     'titles_only': False
 }
 
+master_doc = 'index'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
