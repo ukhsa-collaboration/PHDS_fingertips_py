702b42037e68fbcd1b3e09ee4fdcc5d7ce6b6696
russell.plunkett <russell.plunkett@phe.gov.uk>
2019-04-11 09:51:09 +0100

    Added Licence Changed repo address to public

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     name='fingertips_py',
     version='0.2',
     packages=['fingertips_py'],
-    url='https://gitlab.phe.gov.uk/Russell.Plunkett/fingertips_py.git',
-    license='',
+    url='https://github.com/PublicHealthEngland/PHDS_fingertips_py.git',
+    license='GPL-3.0',
     author='Public Health England',
     author_email='russell.plunkett@phe.gov.uk, phds@phe.gov.uk',
     description='This is a python package to interact with Public Health England\'s Fingertips data tool.'
