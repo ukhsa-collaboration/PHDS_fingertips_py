85bc59ed8efde7799684da61cafc63bf8344f7f0
russell.plunkett <russell.plunkett@phe.gov.uk>
2019-04-11 10:29:08 +0100

    Changed docs file name

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Public Health England'
 author = 'Russell Plunkett'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
