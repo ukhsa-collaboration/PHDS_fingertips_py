fa78c7c8a97b81dde94fede05a725270d73890ff
russell.plunkett <russell.plunkett@phe.gov.uk>
2021-08-21 14:35:46 +0100

    metadata update

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='fingertips_py',
-    version='0.2.2',
+    version='0.2.2.1',
     packages=['fingertips_py'],
     url='https://github.com/PublicHealthEngland/PHDS_fingertips_py.git',
     license='GPL-3.0',
-    author='Public Health England',
+    author='Russell Plunkett, Public Health England',
     author_email='russell.plunkett@phe.gov.uk, phds@phe.gov.uk',
     description='This is a python package to interact with Public Health England\'s Fingertips data tool.'
                 'This can be used to load data from the Fingertips API into python for further manipulation.',
